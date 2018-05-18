b33db3f7e476b03e12e8f65b2c67bde2793a2e36
jtjr01 <jteronesjr@gmail.com>
2018-05-17 21:22:36 -0700

    Added comments and made a small change to the xpath

--- MySel20Proj/src/testng/FirstTestNG.java
+++ MySel20Proj/src/testng/FirstTestNG.java
@@ -25,24 +25,33 @@ public class FirstTestNG {
 	WebDriverWait myWaitVar;
 
 
+	//Launch the browser, set up driver, and the implicit wait
 	@Test(priority = 0)
 	public void launchBrowser() throws IOException {
 		System.out.println("Launching google chrome browswer and navigating to https://www.google.com");
 		System.setProperty("webdriver.chrome.driver", driverPath);
 		driver = new ChromeDriver();
+		//Navigate broswer to specified url
 		driver.get(baseUrl);
+		//Handles implicit waiting 
 		myWaitVar = new WebDriverWait(driver, 10);
+		//Takes a screenshot of results
 		screenshot();
 	}
 
+	//Types automation and searches it using google
 	@Test(priority = 1)
 	public void searchAutomation() throws IOException {
 		System.out.println("Searching for automation on https://www.google.com");
+		//Using xpath to find the search bar
 		WebElement element = driver.findElement(By.xpath("//input[@name='q']"));
 		System.out.println(element);
+		//Simulates typing automation
 		element.sendKeys("automation");
+		//Simulates the search
 		element.submit();
 
+		//Waits for 10 seconds if the title does not match desired search
 		myWaitVar.until(new ExpectedCondition<Boolean>() {
 			public Boolean apply(WebDriver d) {
 				return d.getTitle().toLowerCase().startsWith("automation");
@@ -51,9 +60,12 @@ public class FirstTestNG {
 		screenshot();
 	}
 	
+	//Navigate to the second result
 	@Test(priority = 2)
 	public void visitSecondResultAfterSearch() throws IOException {
-		driver.findElement(By.xpath("//a[@href='http://www.automation.com/']")).click();
+		//Finds the second result and clicks on it
+		driver.findElement(By.xpath("//*[@id='rso']/div[3]/div/div[2]/div/div/h3")).click();
+		//Waits for 10 seconds to make sure the resulting page is the one that was supposed to be clicked on
 		myWaitVar.until(new ExpectedCondition<Boolean>() {
 			public Boolean apply(WebDriver d) {
 				return d.getTitle().startsWith("Automation.com - News & Resources for Manufacturing, Factory & Industrial Automation; Process Control; Motion Control & Instrumentation");
@@ -62,15 +74,21 @@ public class FirstTestNG {
 		screenshot();
 	}
 	
+	//Closes the browser when test are complete
 	@AfterTest
 	public void terminateBrowser() {
 		driver.close();
 	}
 	
+	//Method to handle screenshots
 	public void screenshot() throws IOException{
+		//Creates a blank screenshot when method is called
 		File screenShot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		//Save the screenshot to the folder with the name of the title
 		File screenShotName = new File("C:\\Users\\Jose\\git\\SeleniumPractice\\MySel20Proj\\Screenshots\\" + driver.getTitle() + ".png");
+		//Copies the screenshot taken to new file created
 		FileUtils.copyFile(screenShot, screenShotName);
+		//Add the screenshot to the report log
 		Reporter.log("<br><img src='"+ screenShotName +"' height='400' width='400'/><br>");
 	}
 }
